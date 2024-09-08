b3b582072c2067c635eb0ef12e593611a8a5e166
saeye <saeye42@gmail.com>
2024-09-08 18:36:21 +0900

    feature:Product Update 기능 테스트

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class Product(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product_images/', default="media/product_images/제목_없는_아트워크_82.png")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
