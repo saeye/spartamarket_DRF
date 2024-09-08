dd5a69853af092ad3c4b5c9e0b2f3ba28ef6f2ce
saeye <saeye42@gmail.com>
2024-09-08 20:06:47 +0900

    feature: 좋아요 기능 구현 완료

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='product_like')
 
     def __str__(self):
         return self.title
--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,3 @@
-# products/urls.py
 from django.urls import path
 from . import views
 
@@ -6,5 +5,6 @@ urlpatterns = [
     path('', views.ProductCreateView.as_view()), 
     path('list/', views.ProductListView.as_view()),  
     path('<int:pk>/', views.ProductUpdateView.as_view()),  
-    path('<int:pk>/delete/', views.ProductDeleteView.as_view()),  
+    path('<int:pk>/delete/', views.ProductDeleteView.as_view()),
+    path('<int:pk>/like/', views.product_like),
 ]
