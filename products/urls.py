a963115d152f36193661a3738c4aee7a5d1e6249
saeye <saeye42@gmail.com>
2024-09-08 18:49:23 +0900

    feature:Products 수정기능 완료 및 삭제기능 완료

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.ProductCreateView.as_view()), 
     path('list/', views.ProductListView.as_view()),  
     path('<int:pk>/', views.ProductUpdateView.as_view()),  
-    path('<int:pk>/', views.ProductDeleteView.as_view()),  
+    path('<int:pk>/delete/', views.ProductDeleteView.as_view()),  
 ]
