e65c479056d180c9a7979eb4a050838f154acb91
saeye <saeye42@gmail.com>
2024-09-10 01:15:44 +0900

    Products 페이지네이션 기능  수정하려고  했는데.. accounts 코드정리함..

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     "accounts",
     "products",
@@ -142,7 +141,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1000),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
